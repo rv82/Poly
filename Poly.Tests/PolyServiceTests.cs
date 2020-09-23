f7c1054d12dadef893c21b6f21b17f96c3e78313
Roman Voronov <rv82@ya.ru>
2020-09-23 17:07:13 +0700

    Два новых теста: точка на вершине и точка на ребре

--- Poly.Tests/PolyServiceTests.cs
+++ Poly.Tests/PolyServiceTests.cs
@@ -1,6 +1,5 @@
 using Poly.Models;
 using Poly.Services;
-using System;
 using Xunit;
 
 namespace Poly.Tests
@@ -17,7 +16,7 @@ namespace Poly.Tests
             {
                 new Point{ X=143, Y=100 },
                 new Point{ X=95, Y=270 },
-                new Point{ X=350, Y=317 },
+                new Point{ X=95, Y=317 },
                 new Point{ X=425, Y=155 },
                 new Point{ X=269, Y=57 }
             };
@@ -31,7 +30,7 @@ namespace Poly.Tests
         /// <param name="y">ордината точки</param>
         [Theory]
         [InlineData(287, 156)]
-        [InlineData(333, 254)]
+        [InlineData(322, 160)]
         [InlineData(192, 233)]
         [InlineData(200, 130)]
         [InlineData(344, 120)]
@@ -59,6 +58,46 @@ namespace Poly.Tests
         [InlineData(438, 258)]
         [InlineData(167, 328)]
         public void Point_Not_In_Polygon(double x, double y)
+        {
+            PolygonData data = new PolygonData
+            {
+                Polygon = _polygonPoints,
+                Point = new Point { X = x, Y = y }
+            };
+            bool result = _polyService.InPolygon(data);
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Контрольная точка лежит на одной из вершин многоугольника.
+        /// Для алгоритма она лежит вне многоугольника.
+        /// Через параметры передаются координаты точки, которую нужно проверить.
+        /// </summary>
+        /// <param name="x">абсцисса точки</param>
+        /// <param name="y">ордината точки</param>
+        [Theory]
+        [InlineData(143, 100)]
+        public void Point_On_Polygon_Vertex(double x, double y)
+        {
+            PolygonData data = new PolygonData
+            {
+                Polygon = _polygonPoints,
+                Point = new Point { X = x, Y = y }
+            };
+            bool result = _polyService.InPolygon(data);
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Контрольная точка лежит на одном из рёбер многоугольника.
+        /// Для алгоритма она лежит вне многоугольника.
+        /// Через параметры передаются координаты точки, которую нужно проверить.
+        /// </summary>
+        /// <param name="x">абсцисса точки</param>
+        /// <param name="y">ордината точки</param>
+        [Theory]
+        [InlineData(95, 300)]
+        public void Point_On_Polygon_Edge(double x, double y)
         {
             PolygonData data = new PolygonData
             {
